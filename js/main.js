31d723565256d5c04edbbde44b2342f949409047
Sergio <sergio.barrio.slocker@gmail.com>
2022-12-02 22:30:25 +0100

    full JS cleanup | styling tweaks

--- js/main.js
+++
@@ -1,8 +0,0 @@
-    $(document).ready(function(){
-        var interval = setInterval(function () {
-            var h = $(".main_col").height();
-            $(".left_col").height(h);
-            clearInterval(interval);
-        }, 100);
-
-    });
